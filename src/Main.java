9d573b5b6f5b7a1b4a1f7b3ec1fa464dc0ce523c
brian <robivine99@gmail.com>
2023-02-19 21:44:01 +0300

    java class

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+//
+//        // classes
+//         Cat cat = new Cat();
+//         cat.meow();
+//     }
+//     static class Cat {
+//         String name;
+//         void meow(){
+//             System.out.println(name + " meow...");
+//         }
+//     }
+     /*learning ABOUT Methods.
+     **To consider when building method**
+     -Access Modifier i.e public/private
+     -Optional static e.g static
+     -Return type e.g void/int/char...
+     -Name e.g main or anything
+     -Optional Parameters e.g (String[] args)
+     -Method Body...body is the body that is enclosed by kally brackects
+     -Optional return value..this depend on the return type..if it was "void or int or char"
+      */
+        //creating method that add two numbers
+//    private static int add(int n1, int n2) {
+//        int results = n1 + n2;
+//        return results;
+//    }
+//
+//    public static void main(String[] args) {
+//        int result = add(10, 10);
+//        int result1 = add(10, 100);
+//        System.out.println(result);
+//        System.out.println(result1);
+//    }
+      /*  //If statements
+        if (true) {
+            System.out.println("this code runs when true");
+        } else {
+            System.out.println("this code runs when false");
+        }
+        System.out.println("code outside if statement");
+        System.out.println("hello the above printed line is out of if statement including this as printed at the console");
+    }
+
+        //Loops and Arrays
+        String[] names = {
+                "Wangari",//index0
+                "Munyiva",//index1
+                "Muyaa",//index2
+                "Felistus",//index3
+                "Mirriam",//index4
+                "Brian"//index5
+        };
+        //Using for loops
+        for (int i =0; i< names.length; i++){(we can not use equal sing ("=") because index start from zero but instead we use "<")
+            System.out.println(names[i]);
+        }
+        /*System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
+
+        //Arrays
+       int[] numbers = new int[3];
+       numbers[0] =1;
+       numbers[1] =33;
+       numbers[2] =6;
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length);
+        int[] numbers2 = {0,5,6,8,9,10};
+        System.out.println(Arrays.toString(numbers2));
+        numbers2[1] = 55;
+        System.out.println(Arrays.toString(numbers2));
+        System.out.println(numbers2.length);
+
+        /*
+        //Reference Type / Objects
+        int age = 21;
+        Point pointA = new Point(10, 10);
+        System.out.println("pointA = " + pointA);
+        System.out.println(pointA.distance());*/
+        //Strings
+
+//        String brian = "Brian";
+//        String code = "code";
+//        String brand = brian + code; //( Branding is like concatenating or simples words combining two or more words)
+//        System.out.println("brian + code"); //(Results at the console will be "briancode")
+//        System.out.println(brand.toUpperCase());
+//        System.out.println(brand.toLowerCase(Locale.ROOT));
+//        System.out.println(" ".isEmpty()); //("This would results in booleans at the console,either true or false")
+//        System.out.println(" ".isBlank());
+//    }
+//}
